6ac79e02d83f6b78387593f813a489908d20070f
Pavlukov <sergeybmzh@gmail.com>
2019-03-13 23:26:46 +0300

    Moved code chunk to func.

--- B/taskB.py
+++ B/taskB.py
@@ -1,27 +1,20 @@
-input_num = int(input())
+def calc_first_nums(num, prev_num):
+  while prev_num > 0:
+    tmp = num 
+    num = prev_num
+    prev_num = tmp - num
+  if prev_num < 0:
+    return [round(num), round(tmp)]
+  else:
+    return [round(prev_num), round(num)]
+
+num = int(input())
 div = (1 + 5 ** (1 / 2)) / 2
-num = input_num
+
 prev_num = num // div
+answer_1 = calc_first_nums(num, prev_num)
 
-while prev_num > 0:
-  tmp = num 
-  num = prev_num
-  prev_num = tmp - num
-if prev_num < 0:
-  answer_1 = [round(num), round(tmp)]
-else:
-  answer_1 = [round(prev_num), round(num)]
-
-num = input_num
 prev_num = num // div + 1
-
-while prev_num > 0:
-  tmp = num 
-  num = prev_num
-  prev_num = tmp - num
-if prev_num < 0:
-  answer_2 = [round(num), round(tmp)]
-else:
-  answer_2 = [round(prev_num), round(num)]
+answer_2 = calc_first_nums(num, prev_num)
 
 print("{} {}".format(answer_1[0], answer_1[1])) if sum(answer_1) < sum(answer_2) else print("{} {}".format(answer_2[0], answer_2[1]))
